b894d45f75370bb083209998e037db5383ba29ac
maxkron <maxkron@hotmail.com>
2018-07-09 16:56:35 +0700

    NT

---
+++ Python2/05_PyQt5/tk.py
@@ -0,0 +1,24 @@
+import tkinter
+
+# Создали окно
+root = tkinter.Tk()
+
+t = tkinter.Text()
+
+# Обработка событий - просто функция
+def hello():
+    print("Привет!")
+
+
+# Рисуем компоненты
+b = tkinter.Button(text="Нажми меня!", command=hello)
+
+# Размещение элемента
+b.grid(row=1, column=1)
+
+
+t.grid(row=2, column=2)
+
+root.mainloop()
+
+print("Конец")
